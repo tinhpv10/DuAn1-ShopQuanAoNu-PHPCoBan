a9d029f1eba8e7beb0d9b4252f81d7abc6a88b33
tinhpv10 <117355971+tinhpv10@users.noreply.github.com>
2026-04-16 19:02:06 +0700

    [Model] - Product - Thêm, sửa, xoá

--- Model/Product.php
+++ Model/Product.php
@@ -37,4 +37,51 @@ class Product
         $sth->execute(['khoachinh' => $id]);
         return $sth->fetch(PDO::FETCH_ASSOC);
     }
+
+
+    /**
+     * Hàm này dùng để thêm dữ liệu cho bảng sản phẩm
+     * @param string $name Đây là tên sản phẩm
+     * @param string  $description Đây là mô tả sản phẩm
+     * @param string  $image Đây là đường dẫn hình ảnh
+     * @param int|float $price Đây là giá có thể truyền 2 kiểu int hoặc float
+     * @param bool $status Đây là trạng thái, truyền bool (true/false)
+     * 
+     * @return bool
+     */
+    public function insert(string $name, string $description, string $image, int|float $price, bool $status)
+    {
+        $sql = "INSERT INTO $this->table ( `name`, `description`, `image`, `price`, `status`) 
+        VALUES (?,?,?,?,?);";
+        $stmt = $this->_connect->prepare($sql);
+        return $stmt->execute([$name, $description, $image, $price, $status]);
+    }
+
+    /**
+     * Hàm này dùng để cập nhật dữ liệu cho bảng sản phẩm
+     * @param string $name Đây là tên sản phẩm
+     * @param string  $description Đây là mô tả sản phẩm
+     * @param string  $image Đây là đường dẫn hình ảnh
+     * @param int|float $price Đây là giá có thể truyền 2 kiểu int hoặc float
+     * @param bool $status Đây là trạng thái, truyền bool (true/false)
+     * @param int $id khoá chính
+     * @return bool
+     */
+    public function update(string $name, string $description, string $image, int|float $price, bool $status, int $id)
+    {
+        $sql = "UPDATE $this->table SET `name` = ?, `description` = ?, `image` = ?, `price` = ?, `status` = ? WHERE $this->table.`id` = ?;";
+        $stmt = $this->_connect->prepare($sql);
+        return $stmt->execute([$name, $description, $image, $price, $status, $id]);
+    }
+
+    /**
+     * Hàm này dùng để Xoá dữ liệu cho bảng sản phẩm
+     * @param int $id khoá chính của dữ liệu
+     * @return bool
+     */
+    public function delete(int $id) {
+        $sql = "DELETE FROM products WHERE $this->table.`id` = ?";
+        $stmt = $this->_connect->prepare($sql);
+        return $stmt->execute([$id]);
+    }
 }
